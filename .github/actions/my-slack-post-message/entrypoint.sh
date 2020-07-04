93badd558b54a490723a110589d5e0b80aba8173
goldeneggg <jpshadowapps@gmail.com>
2020-07-04 12:38:20 +0900

    fix slack action

--- .github/actions/my-slack-post-message/entrypoint.sh
+++ .github/actions/my-slack-post-message/entrypoint.sh
@@ -20,6 +20,12 @@ then
   exit 1
 fi
 
+ATTACHMENTS="[]"
+if [ ! -z "${INPUT_ATTACHMENTS}" ]
+then
+  ATTACHMENTS=${INPUT_ATTACHMENTS}
+fi
+
 jsonfile=/tmp/result.json
 
 # See:
@@ -29,13 +35,13 @@ jsonfile=/tmp/result.json
 curl -s -X POST \
      -H "Content-type: application/json; charset=UTF-8" \
      -H "Authorization: Bearer ${INPUT_BOT_TOKEN}" \
-     -d "{\"channel\":\"${INPUT_CHANNEL_ID}\",\"text\":\"${INPUT_TEXT}\",\"attachments\":\"${INPUT_ATTACHMENTS}\"}" \
+     -d "{\"channel\":\"${INPUT_CHANNEL_ID}\",\"text\":\"${INPUT_TEXT}\",\"attachments\":${ATTACHMENTS}}" \
      -o ${jsonfile} \
      https://slack.com/api/chat.postMessage
 
 echo "::set-output name=json::$(cat ${jsonfile})"
 
-grep ':false' ${jsonfile}
+grep '"error":' ${jsonfile}
 if [ $? -eq 0 ]
 then
   echo "slack returned error response"

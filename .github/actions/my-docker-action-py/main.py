c6e9961d1766f3ad90f06c05ac422edc26c8cfdf
vikakaramba <82763082+vikakaramba@users.noreply.github.com>
2021-04-18 16:27:46 +0300

    Delete .github/actions directory

--- .github/actions/my-docker-action-py/main.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-import datetime
-
-
-print(f"Wayway-py!! {sys.argv}")
-
-time = datetime.datetime.now()
-print(f"::set-output name=time::{time}")
--- .github/actions/my-docker-action-sh/cleanup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -l
-
-echo "Post-sh!! $1"
-
-env
-
-echo ">>>>> STATE_ST_ID=${STATE_ST_ID}"
